b6ebbc7c33be4ffbb9a50205c09015322f3dccf9
LSH <dlrkftks123@gmail.com>
2022-11-07 16:24:46 +0900

    membermanage, quitmanage의 detail 표시 구현

--- WorkWith/src/main/webapp/resources/js/manage.js
+++ WorkWith/src/main/webapp/resources/js/manage.js
@@ -94,9 +94,10 @@ function memberManage(is_quit) {
 	}, function(memberList) {
 		var member = "<tr><td>부서</td><td>이름</td><td>직책</td></tr>";
 		$(memberList).each(function(i,memberList){
-			member += "<tr><td><input type='text' name='department' value='"+memberList.department+"'></td>" +
-					"<td><input type='text' name='name' value='"+memberList.name+"'></td>" +
-					"<td><input type='text' name='position' value='"+memberList.position+"'></td>"
+			member += "<tr onclick='detail(\""+memberList.id+"\","+memberList.quit+")'>" +
+					"<td><input type='text' name='department' value='"+memberList.department+"' readonly></td>" +
+					"<td><input type='text' name='name' value='"+memberList.name+"' readonly></td>" +
+					"<td><input type='text' name='position' value='"+memberList.position+"' readonly></td>"
 		})
 		if(is_quit==0){
 			$("#member_simpleData table").html(member);
@@ -105,4 +106,64 @@ function memberManage(is_quit) {
 		}
 		
 	})
+}
+
+// detail을 수정버튼 클릭 시 다시 호출하고 memberManage의 onclick함수를 간단히하기 위해서 id를 매개변수로 줌.
+function detail(thisId,is_quit){
+	var id = thisId;
+	var quit = is_quit;
+	var department = "total";
+	var position = "total";
+	var name = "";
+	$.getJSON("/memberManage", {
+		id : id, 
+		quit : quit,
+		department : department,
+		position : position,
+		name : name
+		}, function(memberDetail){
+			console.log(memberDetail[0])
+			var phone = memberDetail[0].phone.split("-")
+			var email = memberDetail[0].email.split("@")
+			var addr = memberDetail[0].addr.split("/")
+			
+			// 퇴사하지 않은 사원
+			if(is_quit==0){
+				$("#detail_name").val(memberDetail[0].name);
+				$("#detail_id").val(memberDetail[0].id);
+				$("#detail_department").val(memberDetail[0].department);
+				$("#detail_position").val(memberDetail[0].position);
+				$("#detail_startdate").val(memberDetail[0].startdate);
+				$("#detail_emplno").val(memberDetail[0].emplno);
+				$("#detail_email1").val(email[0]);
+				$("#detail_email2").val("@"+email[1]);
+				$("#detail_phone1").val(phone[0]);
+				$("#detail_phone2").val(phone[1]);
+				$("#detail_phone3").val(phone[2]);
+				$("#detail_birth").val(memberDetail[0].birth);
+				$("#detail_addr1").val(addr[0]);
+				$("#detail_addr2").val(addr[1]);
+				$("#detail_addrDetail").val(addr[2]);
+				$("#detail_addrExtra").val(addr[3]);
+			
+			// 퇴사한 사원
+			}else if(is_quit==1){
+				$("#quitDetail_name").val(memberDetail[0].name);
+				$("#quitDetail_id").val(memberDetail[0].id);
+				$("#quitDetail_department").val(memberDetail[0].department);
+				$("#quitDetail_position").val(memberDetail[0].position);
+				$("#quitDetail_startdate").val(memberDetail[0].startdate);
+				$("#quitDetail_emplno").val(memberDetail[0].emplno);
+				$("#quitDetail_email1").val(email[0]);
+				$("#quitDetail_email2").val("@"+email[1]);
+				$("#quitDetail_phone1").val(phone[0]);
+				$("#quitDetail_phone2").val(phone[1]);
+				$("#quitDetail_phone3").val(phone[2]);
+				$("#quitDetail_birth").val(memberDetail[0].birth);
+				$("#quitDetail_addr1").val(addr[0]);
+				$("#quitDetail_addr2").val(addr[1]);
+				$("#quitDetail_addrDetail").val(addr[2]);
+				$("#quitDetail_addrExtra").val(addr[3]);
+			}
+	});
 }
