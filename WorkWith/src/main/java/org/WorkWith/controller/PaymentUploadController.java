604802494aac8736fe5a4a75890cfb79b3015b16
LSH <dlrkftks123@gmail.com>
2022-11-29 11:13:07 +0900

    결재 파일 생성

--- WorkWith/src/main/java/org/WorkWith/controller/PaymentUploadController.java
+++ WorkWith/src/main/java/org/WorkWith/controller/PaymentUploadController.java
@@ -1,5 +1,8 @@
 package org.WorkWith.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PaymentUploadController {
 
 }
--- WorkWith/src/main/java/org/WorkWith/service/PaymentServiceImpl.java
+++ WorkWith/src/main/java/org/WorkWith/service/PaymentServiceImpl.java
@@ -1,5 +1,8 @@
 package org.WorkWith.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentServiceImpl {
 
 }
