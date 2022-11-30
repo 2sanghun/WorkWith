5d2a13ad7eab95b198423960415d961f4b628900
LSH <dlrkftks123@gmail.com>
2022-11-30 16:47:31 +0900

    gd

--- WorkWith/src/main/webapp/WEB-INF/views/payment/paymentRequest.jsp
+++ WorkWith/src/main/webapp/WEB-INF/views/payment/paymentRequest.jsp
@@ -5,38 +5,45 @@
 <head>
 <meta charset="UTF-8">
 <title>결재 요청</title>
+<script type="text/javascript"
+	src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script type="text/javascript"
+	src="../../../resources/js/paymentRequest.js"></script>
+
 </head>
 <body>
 	<input type="hidden" id="request_sendId" value="${id}">
 	<input type="hidden" id="request_sendPosi" value="${position}">
 	<input type="hidden" id="request_sendDep" value="${department}">
 	<input type="hidden" id="request_sendName" value="${name}">
-	<table>
-		<tr>
-			<td><input type="text" name="title" id="paymentTitle"
-				placeholder="제목을 입력하세요" maxlength="40"></td>
-		</tr>
-	</table>
-
-	<textarea id="paymentCon" contentEditable="true"
-		style="min-height: 500px; min-width: 500px; height: auto;"></textarea>
-	<table>
-		<tr>
-			<td><label> 파일첨부 <input type="file" name="uploadFile"
-					id="uploadFile" hidden="" multiple>
-			</label>
-				<div id="lookImage"></div></td>
-			<td style="text-align: left"><div id="uploadResult2"></div></td>
-		</tr>
-	</table>
-	<div id="seniorList">
+	<div>
 		<table>
+			<tr>
+				<td><input type="text" name="title" id="paymentTitle"
+					placeholder="제목을 입력하세요" maxlength="40"></td>
+			</tr>
 		</table>
-	</div>
-	<div id="button">
-		<input type="button" id="sendBtn" value="작성" class="button">
-		<a href="/board/board"><input type="button" value="취소"
-			class="button"></a>
+
+		<textarea id="paymentCon" contentEditable="true"
+			style="min-height: 500px; min-width: 500px; height: auto;"></textarea>
+		<table>
+			<tr>
+				<td><label> 파일첨부 <input type="file" name="paymentFile"
+						id="paymentFile" hidden="" multiple>
+				</label></td>
+				<td style="text-align: left"><div id="uploadResult2"></div>
+					<div id="lookImage"></div></td>
+			</tr>
+		</table>
+		<div id="seniorList">
+			<table>
+			</table>
+		</div>
+		<div id="button">
+			<input type="button" id="sendBtn" value="작성" class="button">
+			<a href="/board/board"><input type="button" value="취소"
+				class="button"></a>
+		</div>
 	</div>
 </body>
 </html>
--- WorkWith/src/main/webapp/resources/js/paymentRequest.js
+++ WorkWith/src/main/webapp/resources/js/paymentRequest.js
@@ -1,4 +1,48 @@
- $(document).ready(function(){
+$(document).ready(function(){
+	// 이미지 파일을 바로 표시하는 함수 작성
+	// reader가 이미지 파일을 읽도록 하고 reader가 이미지 파일(a)을 읽으면 expression이라는 id를 가진 img태그에
+	// src 속성을 부여해 선택한 이미지 파일을 표시함.
+	function setImageFromFile(a, expression) {
+		if (a) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				$(expression).attr('src', e.target.result);
+			}
+			reader.readAsDataURL(a);
+		}
+	}
+	 
+	$('input[name="paymentFile"]').change(function() {
+		var str="";
+		var lookImage="";
+		// 이미지 파일이 아닐경우 image폴더에 있는 이미지를 표시, 이미지 파일이 경우 이미지 태그를 만들고 id를 준다. 이때
+		// src는 주지 않는데 위의 setImageFromFile을 사용하기 위함.
+		// 위의 결과를 str에 저장하고 id가 uploadResult2인 태그에 넣어준다.
+		for(var i=0;i<this.files.length;i++){
+			var fileName = this.files[i].name;
+			var fileType = this.files[i].name;
+			fileType = fileType.slice(fileType.indexOf(".")+1).toLowerCase();
+			if(fileType !="jpg" && fileType !="png" && fileType != "gif" && fileType !="bmp"){
+			     str+="<span><img src='../../../resources/image/folder.png' style='width:30px; height:30px'>"+fileName+"</span>";
+			}else{
+				str +="<span onmouseover='lookImage("+i+")' onmouseout='notImage("+i+")'>"+fileName+"</span><br>";
+				lookImage +="<img id='imageArea"+i+"' style='width:200px; height:100px; display:none'>";
+			}
+		}
+		$("#uploadResult2").html(str);
+		$("#lookImage").html(lookImage);
+		// 위에서 id를 준 이미지 파일을 바로 보이게 하는 반복문
+		for(var i=0; i<this.files.length;i++){
+			fileType = this.files[i].name;
+			fileType = fileType.slice(fileType.indexOf(".")+1).toLowerCase();
+			if(fileType =="jpg" || fileType =="png" || fileType == "gif" || fileType =="bmp"){
+				setImageFromFile(this.files[i], '#imageArea'+i);
+			}else{continue;}
+		}
+	});		
+	
+	
+	
 	/* 첨부파일 공격에 대비하기 위한 업로드 파일 확장자 제한
 		.exe, .zip, .alz 등	-> 첨부 못하게
 		특정 크기 이상의 파일 		-> 첨부 못하게
@@ -44,7 +88,7 @@
 		// 파일 업로드 관련 로직 처리
 		var formData = new FormData();
 		
-		var inputFile = $("input[name='uploadFile']");
+		var inputFile = $("input[name='paymentFile']");
 		// console.log(inputFile);
 		var files = inputFile[0].files;
 		
@@ -57,13 +101,13 @@
 			}
 			
 			// .jsp 의 파일선택을 통해 선택한 파일들을 form태그에 추가
-			formData.append("uploadFile", files[i]);
+			formData.append("paymentFile", files[i]);
 		}
 		
 		// ajax를 통해서 UploadController에 파일 관련 데이터 전송.
 		$.ajax({
 			type : "post",
-			url : "/uploadAjaxAction",
+			url : "/paymentUpload",
 			data : formData,
 			contentType : false,
 			processData : false,
@@ -97,50 +141,6 @@ function writePost(board){
 	})
 }
 
-$(document).ready(function() {
-	// 이미지 파일을 바로 표시하는 함수 작성
-	// reader가 이미지 파일을 읽도록 하고 reader가 이미지 파일(a)을 읽으면 expression이라는 id를 가진 img태그에
-	// src 속성을 부여해 선택한 이미지 파일을 표시함.
-	function setImageFromFile(a, expression) {
-		if (a) {
-			var reader = new FileReader();
-			reader.onload = function(e) {
-				$(expression).attr('src', e.target.result);
-			}
-			reader.readAsDataURL(a);
-		}
-	}
-	 
-	$('input[name="uploadFile"]').change(function() {
-		var str="";
-		var lookImage="";
-		// 이미지 파일이 아닐경우 image폴더에 있는 이미지를 표시, 이미지 파일이 경우 이미지 태그를 만들고 id를 준다. 이때
-		// src는 주지 않는데 위의 setImageFromFile을 사용하기 위함.
-		// 위의 결과를 str에 저장하고 id가 uploadResult2인 태그에 넣어준다.
-		for(var i=0;i<this.files.length;i++){
-			var fileName = this.files[i].name;
-			var fileType = this.files[i].name;
-			fileType = fileType.slice(fileType.indexOf(".")+1).toLowerCase();
-			if(fileType !="jpg" && fileType !="png" && fileType != "gif" && fileType !="bmp"){
-			     str+="<span><img src='../../../resources/image/folder.png' style='width:30px; height:30px'>"+fileName+"</span>";
-			}else{
-				str +="-<span onmouseover='lookImage("+i+")' onmouseout='notImage("+i+")'>"+fileName+"</span><br>";
-				lookImage +="<img id='imageArea"+i+"' style='width:200px; height:100px; display:none'>";
-			}
-		}
-		$("#uploadResult2").html(str);
-		$("#lookImage").html(lookImage);
-		// 위에서 id를 준 이미지 파일을 바로 보이게 하는 반복문
-		for(var i=0; i<this.files.length;i++){
-			fileType = this.files[i].name;
-			fileType = fileType.slice(fileType.indexOf(".")+1).toLowerCase();
-			if(fileType =="jpg" || fileType =="png" || fileType == "gif" || fileType =="bmp"){
-				setImageFromFile(this.files[i], '#imageArea'+i);
-			}else{continue;}
-		}
-	});		
-})
-
 function lookImage(i){
 	var a = $("#imageArea"+i)
 	a.show();
@@ -150,3 +150,4 @@ function notImage(i){
 	var a = $("#imageArea"+i)
 	a.hide();
 }
+
